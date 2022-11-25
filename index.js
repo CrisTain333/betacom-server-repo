0caf6625b0e753efef611acf3288bf90ab27cc0b
Cristain <mitashil99@gmail.com>
2022-11-25 13:41:04 +0600

    role checking completed

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const run = () => {
   const usersCollection = client.db("betacom").collection("users");
 
   try {
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "5h" });
+      res.json(token);
+    });
+    
     app.get("/category", async (req, res) => {
       const query = {};
       const result = await categoryCollection.find(query).toArray();
@@ -41,6 +47,26 @@ const run = () => {
       const result =  await usersCollection.insertOne(user);
       res.send(result);
     })
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    app.get("/users/sellerAccount/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSellerAccount: user?.accountType === "sellerAccount" });
+    });
+    app.get("/users/normalUser/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isNormalUser: user?.accountType === "normalUser" });
+    });
+
 
   } finally {
   }
